ee3fde84bf794c4400c83143feb93e4b5523a39b
frank198 <460454039@qq.com>
2019-02-15 16:54:20 +0800

    feat(logger): 升级log4js 到 4.0 版本

--- examples/log.js
+++ examples/log.js
@@ -1,6 +1,21 @@
-var logger = require('pomelo-logger').getLogger('log', __filename, process.pid);
+const log4jsConfig = require('./log4js');
+const log = require('../index');
+log.configure(log4jsConfig, {
+    serverId : 'test',
+    base     : './examples'
+});
+const logger = log.getLogger('log', __filename);
 
 process.env.LOGGER_LINE = true;
 logger.info('test1');
 logger.warn('test2');
 logger.error('test3');
+logger.fatal('test fatal');
+console.error('console test fatal');
+
+const redisLogger = log.getLogger('redis', __filename);
+// redisLogger.info('redisLogger test1 info');
+// redisLogger.warn('redisLogger test2 warn');
+redisLogger.error('redisLogger test3 error');
+redisLogger.fatal('redisLogger test fatal');
+console.error('redisLogger console test error');
